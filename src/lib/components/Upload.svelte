2e3ae7e5233a033e5f68e46b1cfd54b3fc81fb88
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-02-25 13:41:44 +0100

    fix: `XHR` errors - Deserialize response - Cleaner code

--- src/lib/components/Upload.svelte
+++ src/lib/components/Upload.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+  // @ts-ignore
   import Cookies from "js-cookie";
   import { fade } from "svelte/transition";
+  import { deserialize } from "$app/forms";
+  import type { ActionResult } from "@sveltejs/kit";
   import { formatBytes } from "../utils";
   import {
     buttonClass,
     duration,
     inputFiles,
     maxSize,
-    loading,
     token,
     success,
     error,
@@ -16,6 +18,7 @@
   import FileInput from "./FileInput.svelte";
   import { showSuccess, showError } from "./StatusModal.svelte";
 
+  let uploading = false;
   let progress = 0;
   let uploaded = 0;
   let total = "";
@@ -31,47 +34,53 @@
       showError(`Files cannot be larger than ${maxSizeInMB}MB`);
       return false;
     }
+
     progress = 0;
+    uploading = true;
     total = formatBytes(Array.from($inputFiles).reduce((a, b) => a + b.size, 0));
     Cookies.set("token", $token, { sameSite: "strict" });
 
     const xhr = new XMLHttpRequest();
-    xhr.upload.onload = () => {
-      // TODO
-      progress = 0;
+
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4) {
+        // Parse type of string - response to ActionResult
+        const result: ActionResult = deserialize(xhr.response);
+        switch (result.type) {
+          case "success":
+            showSuccess();
+            break;
+          case "failure":
+            showError("Internal server error");
+            break;
+          case "error":
+            showError(
+              result.error.message ? result.error.message : "Failed to upload file to server"
+            );
+            break;
+        }
+        uploading = false;
+      }
     };
-    xhr.upload.onerror = () => {
-      showError("Failed to upload file to server");
-    };
+
     xhr.upload.onprogress = (event) => {
       if (event.lengthComputable) {
         uploaded = event.loaded;
         progress = (event.loaded / event.total) * 100;
       }
     };
-    // TODO: Fix error
+
+    // @ts-ignore
     xhr.open("POST", this.action);
+
     const data = new FormData(e.target);
     $success = false;
     $error[0] = false;
     // @ts-ignore
     $inputFiles = undefined;
-    xhr.send(data);
-    // TODO: Error handling
-    /*
-      return async ({ result }) => {
-        if (result.type == "success") {
-          showSuccess();
-        } else if (result.type == "failure") {
-          showError("Internal server error");
-        } else if (result.type == "error") {
-          showError(result.error.message ? result.error.message : "Failed to upload file to server");
-        }
-      }
-    */
-  }
 
-  $: if (progress == 100) showSuccess();
+    xhr.send(data);
+  }
 </script>
 
 <form
@@ -81,7 +90,7 @@
   in:fade={{ duration }}
   on:submit|preventDefault={handleSubmit}
 >
-  {#if progress > 0 && progress < 100}
+  {#if (progress > 0 && progress < 100) || uploading}
     <div class="center gap-y-1" transition:fade={{ duration }}>
       <p>{formatBytes(uploaded)} / {total}</p>
       <div class="bg-gray-700 rounded-full drop-shadow h-3 w-72 md:w-96 max-w-full text-center">
