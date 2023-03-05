50f2d349a69b9ad4e45aec64bebddb8a3c7a74ad
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-03-05 19:55:23 +0100

    chore: Update dependencies

--- src/lib/trpc/client.ts
+++ src/lib/trpc/client.ts
@@ -4,7 +4,9 @@ import { createTRPCClient, type TRPCClientInit } from "trpc-sveltekit";
 let browserClient: ReturnType<typeof createTRPCClient<Router>>;
 
 export function trpc(init?: TRPCClientInit) {
-  if (typeof window === "undefined") return createTRPCClient<Router>({ init });
-  if (!browserClient) browserClient = createTRPCClient<Router>();
-  return browserClient;
+  const isBrowser = typeof window !== "undefined";
+  if (isBrowser && browserClient) return browserClient;
+  const client = createTRPCClient<Router>({ init });
+  if (isBrowser) browserClient = client;
+  return client;
 }
