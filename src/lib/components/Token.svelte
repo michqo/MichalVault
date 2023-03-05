f2c5857dea94dc2dedf6640517be295aef33b72c
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-03-05 16:39:05 +0100

    feat: Ability to change token only in dev

--- src/lib/components/Token.svelte
+++ src/lib/components/Token.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import { dev } from "$app/environment";
   import { page } from "$app/stores";
   import { trpc } from "$lib/trpc/client";
   import { buttonClass, token, tokenMaxLength, tokenMinLength } from "../stores";
@@ -38,24 +39,26 @@
 </script>
 
 <div class="center mt-8">
-  <div>
-    <label for="tokenInput" class="text-slate-200 block mb-1 ">Change token</label>
-    <div class="center md:flex-row gap-2 mb-2">
-      <input
-        type="text"
-        id="tokenInput"
-        placeholder="Token"
-        bind:value={newToken}
-        class="px-2 py-2 bg-transparent border border-solid border-gray-200 rounded-md focus:outline-none focus:ring"
-      />
-      <div>
-        <button on:click={changeToken} class="{buttonClass} py-1">Change</button>
-        {#if $token.length < 11}
-          <button on:click={resetToken} class="{buttonClass} py-1">Reset</button>
-        {/if}
+  {#if dev}
+    <div>
+      <label for="tokenInput" class="text-slate-200 block mb-1 ">Change token</label>
+      <div class="center md:flex-row gap-2 mb-2">
+        <input
+          type="text"
+          id="tokenInput"
+          placeholder="Token"
+          bind:value={newToken}
+          class="px-2 py-2 bg-transparent border border-solid border-gray-200 rounded-md focus:outline-none focus:ring"
+        />
+        <div>
+          <button on:click={changeToken} class="{buttonClass} py-1">Change</button>
+          {#if $token.length < 11}
+            <button on:click={resetToken} class="{buttonClass} py-1">Reset</button>
+          {/if}
+        </div>
       </div>
     </div>
-  </div>
+  {/if}
   <p class="text-xl font-bold">Your vault token is</p>
   <code class="text-lg underline select-all">{$token}</code>
   <p class="text-sm text-slate-300">With this token you can access your files</p>
