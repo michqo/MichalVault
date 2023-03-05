0e5f5f1269f0d64782bdf557be9df638f069c3ba
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-03-05 15:36:33 +0100

    feat: Bucket size limit - Vault size limit - More readable +page.server.ts - JSDoc comments in stores.ts

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,18 +1,29 @@
 import { type Writable, writable } from "svelte/store";
 
+/**
+ * Constants
+ */
+
 const buttonClass =
   "text-lg px-3 py-2 rounded-md text-slate-100 bg-white/[.06] border border-slate-300 focus:ring";
 // TODO: Split config variables into separate file
 // Set transition duration in ms
 const duration = 250;
+
 // Limit file upload
-const maxSizeInMB = 20;
-const maxVaultSizeinMB = 20;
+const maxSizeInMB = 30;
 const maxSize = 1048576 * maxSizeInMB;
+const maxVaultSizeinMB = 30;
 const maxVaultSize = 1048576 * maxVaultSizeinMB;
-const maxVaultFilesCount = 10;
+const maxVaultFilesCount = 20;
+const maxBucketSizeInGB = 10;
+const maxBucketSize = 1048576 * 1000 * maxBucketSizeInGB;
 const tokenMinLength = 1;
 const tokenMaxLength = 20;
+
+/**
+ * Svelte stores
+ */
 
 const filesCache: Writable<[Date, Record<string, string>[]]> = writable();
 const inputFiles: Writable<FileList> = writable();
@@ -35,6 +46,8 @@ export {
   maxSize,
   maxVaultSizeinMB,
   maxVaultSize,
+  maxBucketSizeInGB,
+  maxBucketSize,
   tokenMinLength,
   tokenMaxLength,
   maxVaultFilesCount,
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,6 +4,7 @@ import s3 from "$lib/server/connection";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { S3_BUCKET_NAME } from "$env/static/private";
 import {
+  maxBucketSize,
   maxSize,
   maxVaultFilesCount,
   maxVaultSize,
@@ -12,54 +13,69 @@ import {
   tokenMinLength
 } from "$lib/stores";
 
-/*
-async function fetchFiles(token: string): Promise<[any, Record<string, string>][]> {
-  const keys = await redis.sscan(token, 0);
-  if (keys[1].length == 0) return [];
-  const pipeline = redis.pipeline();
-  for (let i = 0; i < keys[1].length; i++) {
-    const key = keys[1][i];
-    if (key.substring(key.length - 1) == "F") continue;
-    pipeline.hgetall(key);
-  }
-  // @ts-ignore
-  return await pipeline.exec();
-}*/
+// Returns vault files size and vault files count
+function countVaultFiles(token: string): Promise<[number, number]> {
+  return new Promise((resolve, reject) => {
+    s3.listObjectsV2(
+      { Bucket: S3_BUCKET_NAME, Delimiter: "/", Prefix: token + "/" },
+      (err, data) => {
+        if (err) return reject(error(500));
+        let totalSize = 0;
+        data?.Contents?.forEach((object) => {
+          totalSize += object.Size!;
+        });
+        // @ts-ignore
+        resolve([data.Contents.length, totalSize]);
+      }
+    );
+  });
+}
 
+// Return error if bucket is full
+function checkBucketSize(filesSize: number): Promise<void> {
+  return new Promise((resolve, reject) => {
+    s3.listObjectsV2({ Bucket: S3_BUCKET_NAME }, (err, data) => {
+      if (err) return reject(error(500));
+      let totalSize = 0;
+      data?.Contents?.forEach((object) => {
+        totalSize += object.Size!;
+      });
+      if (totalSize + filesSize > maxBucketSize) {
+        return reject(error(507, "Database is full"));
+      }
+      resolve();
+    });
+  });
+}
+
+const MAX_VAULT_FILES_MSG = `Vault has too many files, maximum amount of files is ${maxVaultFilesCount}`;
+const MAX_VAULT_SIZE_MSG = `Vault is full, maximum size is ${maxVaultSizeinMB}MB`;
+
+// TODO: More advanced token validation
 export const actions: Actions = {
   default: async ({ cookies, request }) => {
     const token = cookies.get("token");
     if (token == undefined || token.length < tokenMinLength || token.length > tokenMaxLength)
       throw error(400);
     // Pre file upload checks
-    /*
-    const vaultFilesCount = await redis.scard(token);
-    if (vaultFilesCount / 2 >= maxVaultFilesCount)
-      throw error(
-        400,
-        `Vault has too many files, maximum amount of files is ${maxVaultFilesCount}`
-      );
-    */
+    const [vaultFilesCount, vaultFilesSize] = await countVaultFiles(token);
+    if (vaultFilesCount >= maxVaultFilesCount) throw error(400, MAX_VAULT_FILES_MSG);
+    if (vaultFilesSize > maxVaultSize) throw error(413, MAX_VAULT_SIZE_MSG);
+
     const form = await request.formData();
     const files = form.getAll("file_upload") as File[];
     const filesSize = files.reduce((a, b) => a + b.size, 0);
-    // Check if form files size doesn't exceed max size
+
+    // Check size limits
     if (filesSize > maxSize) throw error(400);
+    await checkBucketSize(filesSize);
     // Check if number of files in vault doesn't exceed max size
-    /*
-    if (vaultFilesCount / 2 + files.length > maxVaultFilesCount)
-      throw error(
-        400,
-        `Vault has too many files, maximum amount of files is ${maxVaultFilesCount}`
-      );
-    const vaultFiles = await fetchFiles(token);
-    const vaultSize = vaultFiles.reduce((a, b) => a + parseInt(b[1].size), 0);
-    // Check if files size doesn't exceed max size
-    if (filesSize + vaultSize > maxVaultSize)
-      throw error(413, `Vault is full, maximum size is ${maxVaultSizeinMB}MB`);
-    */
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i];
+    if (vaultFilesCount + files.length > maxVaultFilesCount) throw error(400, MAX_VAULT_FILES_MSG);
+    // Check vault files size
+    if (filesSize + vaultFilesSize > maxVaultSize) throw error(413, MAX_VAULT_SIZE_MSG);
+
+    // Upload files to S3
+    for (let file of files) {
       if (file.size == 0) throw error(400);
       try {
         const arrayBuffer = await file.arrayBuffer();
@@ -78,6 +94,7 @@ export const actions: Actions = {
         return fail(500);
       }
     }
+
     return { success: true };
   }
 };
