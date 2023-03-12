f45d65c687d7396ec0ad3553fe4d518859b1e963
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-03-12 15:18:14 +0100

    feat: Custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
+<div class="flex items-center gap-x-2">
+  <h1 class="text-red-500 text-5xl">{$page.status}:</h1>
+  <p class="text-2xl">{$page.error?.message}</p>
+</div>
