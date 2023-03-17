a92663a43d7d8d4832a0ca318adab2d347c6aec2
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-03-17 10:07:00 +0100

    feat: Enable change token - Input boxes same width

--- src/lib/components/Token.svelte
+++ src/lib/components/Token.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { dev } from "$app/environment";
   import { page } from "$app/stores";
   import { trpc } from "$lib/trpc/client";
   import { token, filesCache } from "../stores";
   import { buttonClass, tokenRegex, TOKEN_ERROR } from "../constants";
   import { showError } from "./StatusModal.svelte";
 
-  const inputDivClass = "center md:flex-row gap-2 mb-2 w-full";
+  const inputDivClass = "center md:flex-row gap-1";
   const labelClass = "text-slate-200 block mb-1";
   const inputClass =
     "w-full px-2 py-2 bg-transparent border border-solid border-gray-200 rounded-md focus:outline-none focus:ring";
@@ -42,28 +41,26 @@
   }
 </script>
 
-<div class="center mt-8 w-full max-w-xs">
-  {#if dev}
-    <div class="w-full">
-      <label for="tokenInput" class={labelClass}>Change token</label>
-      <div class={inputDivClass}>
-        <input
-          type="text"
-          id="tokenInput"
-          placeholder="Token"
-          bind:value={newToken}
-          class={inputClass}
-        />
-        <div>
-          <button on:click={changeToken} class="{buttonClass} py-1">Change</button>
-          {#if $token.length < 11}
-            <button on:click={resetToken} class="{buttonClass} py-1">Reset</button>
-          {/if}
-        </div>
-      </div>
-    </div>
-  {/if}
+<!-- TODO: Input text with label and optional buttons component -->
+
+<div class="center mt-8 md:w-full max-w-xs">
   <div class="w-full">
+    <label for="tokenInput" class={labelClass}>Change token</label>
+    <div class={inputDivClass}>
+      <input
+        type="text"
+        id="tokenInput"
+        placeholder="Token"
+        bind:value={newToken}
+        class={inputClass}
+      />
+      <button on:click={changeToken} class="{buttonClass} py-1 text-base">Change</button>
+      {#if $token.length < 11}
+        <button on:click={resetToken} class="{buttonClass} py-1 text-base">Reset</button>
+      {/if}
+    </div>
+  </div>
+  <div class="w-full mt-2">
     <label for="tokenInput2" class={labelClass}>Go to vault</label>
     <div class={inputDivClass}>
       <input
@@ -73,11 +70,11 @@
         bind:value={redirectToken}
         class={inputClass}
       />
-      <a href="/files/{redirectToken}" class="{buttonClass} py-1">Go</a>
+      <a href="/files/{redirectToken}" class="{buttonClass} py-1 text-base">Go</a>
     </div>
   </div>
 
-  <p class="text-xl font-bold">Your vault token is</p>
+  <p class="text-xl font-bold mt-4">Your vault token is</p>
   <code class="text-lg {$token.length != 0 ? 'underline select-all' : ''}"
     >{$token.length != 0 ? $token : "loading..."}</code
   >
