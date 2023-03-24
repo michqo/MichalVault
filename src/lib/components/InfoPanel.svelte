1e9475b4488e0540317e439a15a0f2772bb1de8c
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-03-24 10:24:00 +0100

    feat: Basic info panel

---
+++ src/lib/components/InfoPanel.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+  import { buttonClass, duration } from "$lib/constants";
+  import { showInfoPanel } from "$lib/stores";
+</script>
+
+<div
+  class="fixed flex justify-center w-full inset-0 z-20 bg-black/[.8]"
+  transition:fade={{ duration }}
+>
+  <div class="w-full h-full max-w-3xl center justify-between">
+    <div class="text-center mt-5">
+      <h1 class="font-medium text-4xl">About</h1>
+      <div class="leading-relaxed tracking-wide mt-3">
+        <p>Web Vault is a simple file hosting service.</p>
+        <p>You can <span class="font-bold">upload, download, delete and browse</span> files.</p>
+        <p>Files are stored in their respective vaults.</p>
+        <p>A vault is bound to a token.</p>
+        <p>Tokens contains of three words separated by dash.</p>
+      </div>
+    </div>
+    <div />
+    <button class="{buttonClass} mb-5" on:click={() => ($showInfoPanel = false)}>Close</button>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { goto } from "$app/navigation";
-  import { inputFiles, filesInput, token } from "$lib/stores";
+  import { inputFiles, filesInput, token, showInfoPanel } from "$lib/stores";
   import { buttonClass } from "$lib/constants";
   import Header from "$lib/components/Header.svelte";
   import Token from "$lib/components/Token.svelte";
   import Upload from "$lib/components/Upload.svelte";
+  import InfoPanel from "$lib/components/InfoPanel.svelte";
   import { showError } from "$lib/components/StatusModal.svelte";
+  import Info from "$lib/svgs/Info.svelte";
 
   onMount(() => {
     // Persistent files between routes
@@ -25,6 +27,14 @@
 <svelte:head>
   <title>Web Vault - Upload files</title>
 </svelte:head>
+
+{#if $showInfoPanel}
+  <InfoPanel />
+{/if}
+
+<button class="fixed top-0 right-0 m-5" on:click={() => ($showInfoPanel = true)}
+  ><Info class="w-10 h-10" /></button
+>
 
 <Header />
 <Token />
