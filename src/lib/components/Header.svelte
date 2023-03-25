80c6c22b2855bb7a348b0d8c99761e25975a52e5
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-03-25 22:32:46 +0100

    feat: Customize header - Add code icon link to the source code

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,20 +1,25 @@
 <script lang="ts">
   import { showInfoPanel } from "$lib/stores";
   import Info from "$lib/svgs/Info.svelte";
+  import Code from "$lib/svgs/Code.svelte";
 </script>
 
-<header class="mt-5 md:mt-20 center">
-  <button class="mb-2 p-2 rounded-md hover:bg-white/[.1]" on:click={() => ($showInfoPanel = true)}
-    ><Info class="w-10 h-10" /></button
-  >
-  <h1>
+<header class="mt-5 md:mt-20">
+  <div class="flex justify-center gap-x-2">
     <a
-      target="_blank"
+      class="p-2 rounded-md hover:bg-white/[.1]"
       href="https://github.com/MichalUSER/web_vault"
-      rel="noreferrer"
-      class="text-4xl md:text-5xl text-center font-medium underline decoration-4"
-      >Web Vault
-    </a>
+      target="_blank"
+      rel="noreferrer"><Code class="w-10 h-10" /></a
+    >
+    <button class="p-2 rounded-md hover:bg-white/[.1]" on:click={() => ($showInfoPanel = true)}
+      ><Info class="w-10 h-10" /></button
+    >
+  </div>
+  <h1
+    class="mt-1 text-indigo-100 text-4xl md:text-5xl text-center font-semibold tracking-wider underline decoration-3"
+  >
+    Web Vault
   </h1>
-  <p class="mt-1 text-center text-slate-300 font-light">Anonymous file storage</p>
+  <p class="mt-1 text-center text-slate-300 font-light">File hosting service</p>
 </header>
---
+++ src/lib/svgs/Code.svelte
@@ -0,0 +1,16 @@
+<script>
+  let className = void 0;
+  export { className as class };
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  height="48"
+  width="48"
+  viewBox="0 96 960 960"
+  class={className}
+  fill="currentColor"
+  ><path
+    d="M320 814 80 574l242-242 43 43-199 199 197 197-43 43Zm318 2-43-43 199-199-197-197 43-43 240 240-242 242Z"
+  /></svg
+>
