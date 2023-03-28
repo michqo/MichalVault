892bea0632726bdfed7d15a5f44aefd17c3c1ad1
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-03-28 12:24:55 +0200

    fix: Center `+error` page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,7 @@
   import { page } from "$app/stores";
 </script>
 
-<div class="flex items-center gap-x-2">
+<div class="fixed w-full h-full flex items-center justify-center gap-x-2">
   <h1 class="text-red-500 text-5xl">{$page.status}:</h1>
   <p class="text-2xl">{$page.error?.message}</p>
 </div>
