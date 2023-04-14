92567d425b341749d24022dd93a2a38659e0ce5b
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-04-14 16:18:46 +0200

    fix: Sidebar rounded corners

--- src/lib/components/controls/Sidebar.svelte
+++ src/lib/components/controls/Sidebar.svelte
@@ -8,7 +8,7 @@
   class="fixed z-10 bottom-0 md:inset-0 h-fit md:w-fit md:h-full flex justify-center md:items-center"
 >
   <div
-    class="flex flex-row md:flex-col gap-y-2 px-3 py-2 bg-gray-900 border border-b-0 md:border-l-0 md:border-b border-slate-700 rounded-t-md md:rounded-t-none md:rounded-r-md drop-shadow-xl"
+    class="flex flex-row md:flex-col gap-y-2 px-3 py-2 bg-gray-900 border border-b-0 md:border-l-0 md:border-b border-slate-700 rounded-t-md md:rounded-tl-none md:rounded-r-md drop-shadow-xl"
   >
     <slot />
   </div>
