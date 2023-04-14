938d735f8f7c96bd6856edc37e1ea7a81cf7caac
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-04-14 22:59:19 +0200

    feat: Update info panel

--- src/lib/components/InfoPanel.svelte
+++ src/lib/components/InfoPanel.svelte
@@ -27,22 +27,26 @@
 
 <div
   class="fixed center w-full inset-0 z-20 overflow-y-auto bg-transparent backdrop-blur-xl"
-  transition:fade={{duration}}
+  transition:fade={{ duration }}
 >
   <button class="mt-10 p-1 rounded-md hover:bg-white/[.1]" on:click={() => ($showInfoPanel = false)}
     ><Close class="w-12 h-12 text-red-500" /></button
   >
   <div class="w-full h-full max-w-xs md:max-w-3xl center gap-y-3 justify-center text-center">
     <h1 class="font-bold text-5xl tracking-widest">About</h1>
-    <div class="leading-relaxed tracking-wide mt-5">
-      <p>Web Vault is a simple file hosting service.</p>
-      <p>You can <span class="font-medium">upload, download, delete and browse</span> files.</p>
-      <p>Files are stored in their respective vaults.</p>
-      <p>A vault is bound to a token.</p>
-      <p>Tokens are contained of three words separated by a dash.</p>
+    <div class="leading-relaxed tracking-wide mt-3">
+      <p>MichalStore is a simple file hosting service.</p>
+      <p>
+        Think of it as <span class="font-medium">one drive, google cloud</span> etc, but free and open
+        source.
+      </p>
     </div>
-    <div class="flex flex-col gap-y-4">
-      <h2 class="font-medium text-3xl mt-5">Instance settings</h2>
+    <p class="mt-4">
+      <span class="font-bold">TIP: </span>Press
+      <code class="p-1 bg-gray-800 rounded-md tracking-widest">Ctrl + V</code> to paste image from clipboard
+    </p>
+    <div class="flex flex-col gap-y-4 mt-8">
+      <h2 class="font-medium text-3xl">Instance settings</h2>
       <p>Maximum file upload size: <span class="font-medium">{maxSizeInMB} MB</span></p>
       <p>Maximum vault files size: <span class="font-medium">{maxVaultSizeinMB} MB</span></p>
       <p>Maximum no. of files in vault: <span class="font-medium">{maxVaultFilesCount}</span></p>
