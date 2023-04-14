e7cbca1d709e7d4521e5cd998040206184e5cd17
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-04-14 08:18:19 +0200

    feat: Sidebar above overlays

--- src/lib/components/controls/Sidebar.svelte
+++ src/lib/components/controls/Sidebar.svelte
@@ -5,7 +5,7 @@
 
 <div
   transition:fade={{ duration }}
-  class="fixed bottom-0 md:inset-0 h-fit md:w-fit md:h-full flex justify-center md:items-center"
+  class="fixed z-10 bottom-0 md:inset-0 h-fit md:w-fit md:h-full flex justify-center md:items-center"
 >
   <div
     class="flex flex-row md:flex-col gap-y-2 px-3 py-2 bg-gray-900 border border-b-0 md:border-l-0 md:border-b border-slate-700 rounded-t-md md:rounded-t-none md:rounded-r-md drop-shadow-xl"
